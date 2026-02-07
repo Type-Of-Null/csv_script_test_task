24b49358d7cbd3c9cf9017e2177f78630d3cfaf8
Дмитрий <dimasafronov55@mail.ru>
2026-02-07 15:40:10 +0300

    feat: complete report display logic

--- src/report.py
+++ src/report.py
@@ -6,6 +6,7 @@ from src.service.operate_csv import OperateCSV
 from src.service.operate_data import OperateData
 
 
+# Загрузка и обработка CSV файлов
 def load_and_process_data(file_paths: list[str]) -> dict[str, float]:
     all_data = []
 
@@ -23,7 +24,19 @@ def load_and_process_data(file_paths: list[str]) -> dict[str, float]:
     return sorted_gdp
 
 
+# Вывод отчета в консоль
+def display_report(report_name: str, data: dict[str, float]) -> None:
+    table_data = [[country, gdp] for country, gdp in data.items()]
+    headers = ["country", "gdp"]
+
+    print(f"\n Отчет {report_name}")
+    print(tabulate(table_data, headers=headers, tablefmt="outline", floatfmt=".2f"))
+    print()
+
+
+# Точка входа в приложение
 def main() -> None:
+
     parser = argparse.ArgumentParser(
         description="Обработка csv файлов и генерация отчетов"
     )
@@ -39,6 +52,17 @@ def main() -> None:
         default="average-gdp",
         help="Вид отчета",
     )
+
+    args = parser.parse_args()
+
+    # Выбор логики в зависимости от типа отчета
+    # При масштабировании можно добавлять необходимую реализацию
+    if args.report == "average-gdp":
+        data = load_and_process_data(args.files)
+        display_report(args.report, data)
+    else:
+        print(f"Неправильный тип отчета: {args.report}")
+        sys.exit(1)
 
 
 if __name__ == "__main__":
